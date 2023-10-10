5aae960b6f219bce0a89dafd71e3ad2e70eb0a8e
Vishruth Rao <116237865+Powerlax@users.noreply.github.com>
2023-10-09 20:02:59 -0700

    Update p1.sh
    Signed-off-by: Vishruth Rao <116237865+Powerlax@users.noreply.github.com>

--- p1.sh
+++ p1.sh
@@ -6,6 +6,12 @@ curl -s https://packagecloud.io/install/repositories/github/git-lfs/script.deb.s
 sudo apt-get install git-lfs
 sudo apt install python3
 sudo apt install python3-pip
+type -p curl >/dev/null || (sudo apt update && sudo apt install curl -y)
+curl -fsSL https://cli.github.com/packages/githubcli-archive-keyring.gpg | sudo dd of=/usr/share/keyrings/githubcli-archive-keyring.gpg \
+&& sudo chmod go+r /usr/share/keyrings/githubcli-archive-keyring.gpg \
+&& echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/githubcli-archive-keyring.gpg] https://cli.github.com/packages stable main" | sudo tee /etc/apt/sources.list.d/github-cli.list > /dev/null \
+&& sudo apt update \
+&& sudo apt install gh -y
 cd yolov5
 pip3 install -r requirements.txt comet_ml
 pip3 install roboflow
